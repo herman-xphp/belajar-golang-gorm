00e8d69118d9b405842313ae7dcfb794cb485404
herman-xphp <hermanxphp@gmail.com>
2025-11-08 22:30:50 +0800

    Raw SQL

--- gorm_test.go
+++ gorm_test.go
@@ -23,3 +23,70 @@ var db = OpenConnection()
 func TestOpenConnection(t *testing.T) {
 	assert.NotNil(t, db)
 }
+
+func TestExecSQL(t *testing.T) {
+	err := db.Exec("insert into sample(id, name) values (?,?)", "1", "Budi").Error
+	assert.Nil(t, err)
+
+	err = db.Exec("insert into sample(id, name) values (?,?)", "2", "Anto").Error
+	assert.Nil(t, err)
+
+	err = db.Exec("insert into sample(id, name) values (?,?)", "3", "Wahyu").Error
+	assert.Nil(t, err)
+
+	err = db.Exec("insert into sample(id, name) values (?,?)", "4", "Arif").Error
+	assert.Nil(t, err)
+}
+
+type Sample struct {
+	Id   string
+	Name string
+}
+
+func TestRawSQL(t *testing.T) {
+	var sample Sample
+	err := db.Raw("select id, name from sample where id = ?", "1").Scan(&sample).Error
+	assert.Nil(t, err)
+	assert.Equal(t, "Budi", sample.Name)
+
+	var samples []Sample
+	err = db.Raw("select id, name from sample").Scan(&samples).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(samples))
+}
+
+func TestSqlRows(t *testing.T) {
+	rows, err := db.Raw("select id, name from sample").Rows()
+	assert.Nil(t, err)
+	defer rows.Close()
+
+	var samples []Sample
+	for rows.Next() {
+		var id string
+		var name string
+
+		err := rows.Scan(&id, &name)
+		assert.Nil(t, err)
+
+		samples = append(samples, Sample{
+			Id:   id,
+			Name: name,
+		})
+	}
+
+	assert.Equal(t, 4, len(samples))
+}
+
+func TestScanRows(t *testing.T) {
+	rows, err := db.Raw("select id, name from sample").Rows()
+	assert.Nil(t, err)
+	defer rows.Close()
+
+	var samples []Sample
+	for rows.Next() {
+		err := db.ScanRows(rows, &samples)
+		assert.Nil(t, err)
+	}
+
+	assert.Equal(t, 4, len(samples))
+}
