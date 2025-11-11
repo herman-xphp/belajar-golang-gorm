02739aed4cea46d80265bc19c3acea8135ac5598
herman-xphp <hermanxphp@gmail.com>
2025-11-11 23:43:08 +0800

    Update

--- gorm_test.go
+++ gorm_test.go
@@ -309,3 +309,36 @@ func TestQueryNonModel(t *testing.T) {
 	assert.Equal(t, 14, len(users))
 	fmt.Println(users)
 }
+
+func TestUpdate(t *testing.T) {
+	user := User{}
+	err := db.Take(&user, "id = ?", "1").Find(&user).Error
+	assert.Nil(t, err)
+
+	user.Name.FirstName = "Budi"
+	user.Name.MiddleName = ""
+	user.Name.LastName = "Nugraha"
+	user.Password = "rahasia123"
+
+	err = db.Save(&user).Error
+	assert.Nil(t, err)
+}
+
+func TestUpdateSelectedColumns(t *testing.T) {
+	err := db.Model(&User{}).Where("id = ?", "1").Updates(map[string]any{
+		"middle_name": "",
+		"last_name":   "",
+	}).Error
+	assert.Nil(t, err)
+
+	err = db.Model(&User{}).Where("id = ?", "1").Update("password", "apaya?").Error
+	assert.Nil(t, err)
+
+	err = db.Where("id = ?", "1").Updates(User{
+		Name: Name{
+			FirstName: "Eko",
+			LastName:  "khannedy",
+		},
+	}).Error
+	assert.Nil(t, err)
+}
